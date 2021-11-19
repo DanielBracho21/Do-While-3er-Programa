3ed4571b73a2c2cb380036ac2cf3535e92773cd7
arash <arash@ARASHITORA>
2021-11-19 01:32:38 -0400

    Add project files.

---
+++ Do While 3er Programa/Do While 3er Programa.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() // Visualizara numeros del 1 - 9
+{
+    int digito = 0;
+    do
+
+        printf("%d ", digito++);
+    while (digito <= 9);
+
+}
